c9c2516cc7cfc4580f012565f1e53529f90c440f
Clovis Durand <cd.clovel19@gmail.com>
2021-09-20 16:22:03 +0200

    Factored the user input management in a separate function
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/codePush.ts
+++ src/codePush.ts
@@ -431,6 +431,58 @@ class CodePush implements CodePushCapacitorPlugin {
       syncCallback && syncCallback(null, SyncStatus.UPDATE_INSTALLED);
     };
 
+    const manageUserInput = async (remotePackage: RemotePackage): Promise<boolean> => {
+      if (syncOptions.updateDialog) {
+        CodePushUtil.logMessage("Awaiting user action.");
+        syncCallback && syncCallback(null, SyncStatus.AWAITING_USER_ACTION);
+
+        const dlgOpts: UpdateDialogOptions = <UpdateDialogOptions>syncOptions.updateDialog;
+
+        if (remotePackage.isMandatory) {
+          /* Alert user */
+          const message = dlgOpts.appendReleaseDescription ?
+            dlgOpts.mandatoryUpdateMessage + dlgOpts.descriptionPrefix + remotePackage.description :
+            dlgOpts.mandatoryUpdateMessage;
+
+          await Dialog.alert(
+            {
+              message,
+              title: dlgOpts.updateTitle,
+              buttonTitle: dlgOpts.mandatoryContinueButtonLabel
+            }
+          );
+          CodePushUtil.logMessage("User dismissed the mandatory update dialog.");
+
+          return true; /* Should update */
+        } else {
+          /* Confirm update with user */
+          const message = dlgOpts.appendReleaseDescription ?
+            dlgOpts.optionalUpdateMessage + dlgOpts.descriptionPrefix + remotePackage.description
+            : dlgOpts.optionalUpdateMessage;
+
+          const confirmResult: ConfirmResult = await Dialog.confirm({
+            message,
+            title: dlgOpts.updateTitle,
+            okButtonTitle: dlgOpts.optionalInstallButtonLabel,
+            cancelButtonTitle: dlgOpts.optionalIgnoreButtonLabel
+          });
+
+          if (confirmResult.value === true) {
+            CodePushUtil.logMessage("User accepted the update.");
+            return true; /* Should update */
+          } else {
+            /* Cancel */
+            CodePushUtil.logMessage("User cancelled the update.");
+            syncCallback && syncCallback(null, SyncStatus.UPDATE_IGNORED);
+            return false; /* Should not update, the user declined the update */
+          }
+        }
+      } else {
+        /* No user interaction */
+        return true; /* Should update */
+      }
+    };
+
     const onUpdate = async (remotePackage: RemotePackage) => {
       if (remotePackage === null) {
         /* Then the app is up to date */
@@ -440,56 +492,17 @@ class CodePush implements CodePushCapacitorPlugin {
           CodePushUtil.logMessage("An update is available, but it is being ignored due to have been previously rolled back.");
           syncCallback && syncCallback(null, SyncStatus.UPDATE_IGNORED);
         } else {
-          if (syncOptions.updateDialog) {
-            CodePushUtil.logMessage("Awaiting user action.");
-            syncCallback && syncCallback(null, SyncStatus.AWAITING_USER_ACTION);
+          /* Check if user input is required.
+           * if it's the case, get user input */
+          const lShouldUpdate: boolean = await manageUserInput(remotePackage);
 
-            const dlgOpts: UpdateDialogOptions = <UpdateDialogOptions>syncOptions.updateDialog;
-
-            if (remotePackage.isMandatory) {
-              /* Alert user */
-              const message = dlgOpts.appendReleaseDescription ?
-                dlgOpts.mandatoryUpdateMessage + dlgOpts.descriptionPrefix + remotePackage.description :
-                dlgOpts.mandatoryUpdateMessage;
-
-              await Dialog.alert(
-                {
-                  message,
-                  title: dlgOpts.updateTitle,
-                  buttonTitle: dlgOpts.mandatoryContinueButtonLabel
-                }
-              );
-              CodePushUtil.logMessage("User dismissed the mandatory update dialog.");
-            } else {
-              /* Confirm update with user */
-              const message = dlgOpts.appendReleaseDescription ?
-                dlgOpts.optionalUpdateMessage + dlgOpts.descriptionPrefix + remotePackage.description
-                : dlgOpts.optionalUpdateMessage;
-
-              const confirmResult: ConfirmResult = await Dialog.confirm({
-                message,
-                title: dlgOpts.updateTitle,
-                okButtonTitle: dlgOpts.optionalInstallButtonLabel,
-                cancelButtonTitle: dlgOpts.optionalIgnoreButtonLabel
-              });
-
-              if (confirmResult.value === true) {
-                CodePushUtil.logMessage("User accepted the update.");
-              } else {
-                /* Cancel */
-                CodePushUtil.logMessage("User cancelled the update.");
-                syncCallback && syncCallback(null, SyncStatus.UPDATE_IGNORED);
-              }
-            }
-          } else {
-            /* No user interaction, nothing to do */
+          if(lShouldUpdate) {
+            /* Download */
+            const localPackage = await downloadUpdate(remotePackage);
+            /* Install */
+            const installMode = await installUpdate(localPackage);
+            onInstallSuccess(installMode);
           }
-
-          /* Download */
-          const localPackage = await downloadUpdate(remotePackage);
-          /* Install */
-          const installMode = await installUpdate(localPackage);
-          onInstallSuccess(installMode);
         }
       }
     };
